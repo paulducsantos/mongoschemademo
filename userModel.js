d0e6c3e0134f04fd97f09e086d8c24f51fc47f14
Paul Santos <paulmichael.a.santos@gmail.com>
2016-03-12 11:34:29 -0500

    added date

--- userModel.js
+++ userModel.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt')
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
   //Code goes in here
   username: {
     type: String,
+    trim: true,
     required: true
   },
   password: {
@@ -14,8 +16,13 @@ var UserSchema = new Schema({
   },
   email: {
     type: String,
+    trim: true,
     required: true,
     match: [/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i, 'bad password']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 });
 
